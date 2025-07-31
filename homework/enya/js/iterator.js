b961bf4293622e65e6386e28c059ab0080f71536
Enya Chen <enya.chen@104.com.tw>
2025-07-31 09:23:48 +0800

    fix(iterator): modify parameter checks and add type validation

--- homework/enya/js/iterator.js
+++ homework/enya/js/iterator.js
@@ -1,38 +1,27 @@
-const idIterator = createUserIdIterator(1000, 1006);
-// console.log(idIterator[Symbol.iterator]().next());
-// console.log(idIterator[Symbol.iterator]().next());
-// console.log(idIterator[Symbol.iterator]().next());
-console.log(idIterator.next()); // { value: 1000, done: false }
-// console.log(idIterator.next()); // { value: 1002, done: false }
-// console.log(idIterator.next()); // { value: 1004, done: false }
-// console.log(idIterator.next()); // { value: 1006, done: false }
-// console.log(idIterator.next()); // { value: undefined, done: true }
-
-function createUserIdIterator(start, end) {
-  let currentValue = start;
-  let oldValue;
+function createUserIdIterator(startNumber, endNumber) {
+  let currentNumber = startNumber;
 
   return {
     [Symbol.iterator]() {
       return this;
     },
     next() {
-      if (currentValue <= end && currentValue % 2 === 0) {
-        oldValue = currentValue;
-        currentValue += 2;
-        return { value: oldValue, done: false };
+      try {
+        if (typeof startNumber !== "number" || typeof endNumber !== "number") {
+          throw new Error("invalid input");
+        }
+
+        currentNumber % 2 === 0 ? currentNumber : currentNumber++;
+        const isProgress =
+          currentNumber <= endNumber && currentNumber % 2 === 0;
+
+        return {
+          value: isProgress ? (currentNumber += 2) - 2 : undefined,
+          done: !isProgress,
+        };
+      } catch (e) {
+        console.error(e);
       }
-      return { done: true };
     },
   };
 }
-
-// for (let iterator of idIterator) {
-//   console.log(iterator);
-// }
-
-// 1. start end ,data type number
-// 2. 定義 symbol iterator
-// 3. return object 需要一個 next method
-// 4. next() return object {value:number, done:boolean}
-// 5. 如果是偶數 return current += 2
